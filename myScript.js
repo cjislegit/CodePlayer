aa0449d881efb6746f448174aad85be885397387
cjislegit <carlosjramirez11@gmail.com>
2018-05-27 21:45:21 -0700

    Added iframe and updates styles and css to match

--- myScript.js
+++ myScript.js
@@ -1,11 +1,25 @@
 $('input').click(function(){
     $(this).toggleClass('selected');
-    alert($(this).attr('id'));
-})
+    displayColumn($(this).attr('id'));
+});
 
 function displayColumn(buttonId){
     switch(buttonId){
         case 'htmlButton':
+        $('#html').toggle();
+        break;
+        case 'cssButton':
+        $('#css').toggle();
+        break;
+        case 'javascriptButton':
+        $('#javascript').toggle();
+        break;
+        case 'outputButton':
+        $('#output').toggle();
         break;
     }
 }
+
+$('textarea').on('change keyup paste', function() { 
+
+ });
